d064c435f3bd024199e3e02f33029076d849217e
xiangshangzhi <xiangshangzhi@163.com>
2023-12-15 15:53:37 +0800

    feat: 共享模块服务

--- src/lions/lions.module.ts
+++ src/lions/lions.module.ts
@@ -5,5 +5,8 @@ import { LionsController } from './lions.controller';
 @Module({
   controllers: [LionsController],
   providers: [LionsService],
+  // 共享模块 可以在其他几个模块中共享LionsService的一个实例
+  // 所有导入LionsModule的模块都可以使用LionsService
+  exports: [LionsService],
 })
 export class LionsModule {}
