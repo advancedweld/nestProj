65f417d777395ac8095cdbf7e225491a267ae45c
xiangshangzhi <xiangshangzhi@163.com>
2023-12-15 09:46:11 +0800

    feat: 路由参数

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,17 +1,41 @@
-import { Controller, Get, Post, Req, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  HttpCode,
+  Param,
+  Header,
+} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Post()
   @HttpCode(200)
+  // @header 自定义响应头
+  @Header('Cache-Control', 'none')
+  @Header('my-token', 'xiangshangzhi')
   create(): string {
     return 'This action adds a new cat';
   }
 
   @Get()
+  // @req 装饰器获取请求头
   findAll(@Req() request: Request): string {
     console.log('@@@request', request.body);
     return 'This action returns all cats';
   }
+
+  // 带参数路由
+  @Get(':id')
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  }
+
+  // findOne(@Param('id') id: string): string {
+  //   console.log(id);
+  //   return `This action returns a #${id} cat`;
+  // }
 }
