ac49724b6b8caf94460dbb5127904b2ec5ccfbe4
xiangshangzhi <xiangshangzhi@163.com>
2023-12-20 10:30:15 +0800

    feat: 设置全局路由前缀

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { LoggerMiddleware, logger } from './middleware/logger.middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.setGlobalPrefix('api-x');
   // 应用全局中间件
-  // app.use(logger);
+  app.use(logger);
   await app.listen(3000);
   console.log('Server running on port 3000');
 }
