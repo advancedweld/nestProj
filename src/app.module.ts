56a82f19f7dad1778f70e8f88cad4e50a9d88f9b
xiangshangzhi <xiangshangzhi@163.com>
2023-12-22 14:22:14 +0800

    feat: init auth module, import bcryptjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { config } from './ormconfig';
 
 import { AuthorModule } from './author/author.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     // 接入mysql数据库
@@ -22,6 +23,7 @@ import { UserModule } from './user/user.module';
     PhotosModule,
     AuthorModule,
     UserModule,
+    AuthModule,
   ],
   // 控制器始终属于一个模块，这就是我们在 @Module() 装饰器中包含 controllers 数组的原因。 由于我们还没有定义除根 AppModule 之外的任何其他模块，我们将使用它来引入 CatsController
   controllers: [AppController, CatsController, TranslateController],
