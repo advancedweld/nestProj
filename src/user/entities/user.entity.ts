a79b6fb3e4118be516702c7c379bca39b8035979
xiangshangzhi <xiangshangzhi@163.com>
2023-12-22 13:50:52 +0800

    feat: 修改user字段

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -5,15 +5,20 @@ import {
   OneToMany,
   BaseEntity,
 } from 'typeorm';
-
+import { IsInt, IsString, isString } from 'class-validator';
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
+  @IsString()
   username: string;
 
   @Column()
+  @IsString()
   password: string;
+
+  @Column({ nullable: true })
+  avatar: string;
 }
