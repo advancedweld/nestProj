aedd7781f101c427a4d6e78c0eae4c991b7fb70e
xiangshangzhi <xiangshangzhi@163.com>
2024-07-03 10:33:50 +0800

    feat: bugfix，暂时无法处理cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
   Body,
   Req,
   Res,
+  HttpException,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
@@ -22,22 +23,8 @@ export class AuthController {
   // @UseGuards(AuthGuard('local'))
   @UseInterceptors(ClassSerializerInterceptor)
   @Post('login')
-  async login(@Body() loginUserDto: LoginUserDto, @Res() res: Response) {
+  async login(@Body() loginUserDto: LoginUserDto) {
     const result = await this.authService.authLogin(loginUserDto);
-    if ('accessToken' in result) {
-      res.cookie('customer_token', 'xiangshangzhi6666', {
-        httpOnly: true,
-        // secure: true,
-        sameSite: 'none',
-
-        maxAge: 3600000,
-      });
-      return res.json({
-        userName: result.userName,
-        role: result.role,
-      });
-    } else {
-      return res.status(401).json(result);
-    }
+    return result;
   }
 }
