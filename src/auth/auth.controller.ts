8b1bd5fe1879503c769cfd2e02a776dce977079d
xiangshangzhi <xiangshangzhi@163.com>
2024-07-03 09:46:42 +0800

    feat: 登录接口设置cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,7 +7,9 @@ import {
   Get,
   Body,
   Req,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 // import { AuthGuard } from '@nestjs/passport';
 import { LoginUserDto } from '../user/dto/login-user.dto';
@@ -20,8 +22,22 @@ export class AuthController {
   // @UseGuards(AuthGuard('local'))
   @UseInterceptors(ClassSerializerInterceptor)
   @Post('login')
-  async login(@Body() loginUserDto: LoginUserDto) {
+  async login(@Body() loginUserDto: LoginUserDto, @Res() res: Response) {
     const result = await this.authService.authLogin(loginUserDto);
-    return result;
+    if ('accessToken' in result) {
+      res.cookie('customer_token', 'xiangshangzhi6666', {
+        httpOnly: true,
+        // secure: true,
+        sameSite: 'none',
+
+        maxAge: 3600000,
+      });
+      return res.json({
+        userName: result.userName,
+        role: result.role,
+      });
+    } else {
+      return res.status(401).json(result);
+    }
   }
 }
